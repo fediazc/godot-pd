bde3f8713761af8ecc4308661ff6c06792f57bf8
Fernando Diaz <fediaz.dev@gmail.com>
2024-10-17 12:15:07 -0400

    fix bugs in demo project and link in README

--- demo/scenes/main.gd
+++ demo/scenes/main.gd
@@ -7,31 +7,6 @@ var playing = false
 
 func _ready():
 	begin_playback()
-
-func _process(_delta):
-	var rect_size = get_viewport().get_visible_rect().size
-	var mouse_pos = get_global_mouse_position()
-	var freq = clampf(1 - mouse_pos.y / rect_size.y, 0, 1) * 880
-	
-	freq_label.text = "Frequency: %.0f Hz" % freq
-	
-	if Input.is_action_just_pressed("play_tone") and playing:
-		pd_playback.send_bang("bang")
-	
-	if Input.is_action_just_pressed("toggle_play"):
-		if playing:
-			stop_playback()
-		else:
-			print()
-			begin_playback()
-	
-	if playing:
-		pd_playback.send_float("freq", freq)
-
-func begin_playback():
-	AudioStreamPlayerPD.play()
-	pd_playback = AudioStreamPlayerPD.get_stream_playback()
-	
 	pd_playback.add_patch("test-array.pd", "./pd")
 	var sz := pd_playback.get_array_size("array")
 	var arr := pd_playback.read_array("array")
@@ -56,6 +31,29 @@ func begin_playback():
 	pd_playback.add_patch("test-send.pd", "./pd")
 	pd_playback.send_list("list", ["test", 1, 2, 3])
 
+func _process(_delta):
+	var rect_size = get_viewport().get_visible_rect().size
+	var mouse_pos = get_global_mouse_position()
+	var freq = clampf(1 - mouse_pos.y / rect_size.y, 0, 1) * 880
+	
+	freq_label.text = "Frequency: %.0f Hz" % freq
+	
+	if Input.is_action_just_pressed("play_tone") and playing:
+		pd_playback.send_bang("bang")
+	
+	if Input.is_action_just_pressed("toggle_play"):
+		if playing:
+			stop_playback()
+		else:
+			begin_playback()
+	
+	if playing:
+		pd_playback.send_float("freq", freq)
+
+func begin_playback():
+	AudioStreamPlayerPD.play()
+	pd_playback = AudioStreamPlayerPD.get_stream_playback()
+	
 	playing = true
 
 func stop_playback():
